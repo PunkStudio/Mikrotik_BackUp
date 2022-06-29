8e5157ea097fbd2a3b56f279560a3312cbc85df4
PunkStudio <punkstudio.vbg@gmail.com>
2022-06-29 15:33:09 +0300

    edit requirements.txt

--- make_backup.py
+++ make_backup.py
@@ -5,7 +5,6 @@ try:
     import xml.etree.cElementTree as ET
 except ImportError:
     import xml.etree.ElementTree as ET
-import netmiko
 from netmiko import ConnectHandler
 import time
 from colorama import init, Fore, Back, Style
